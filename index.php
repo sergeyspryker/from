99196e8ad86bbfab319d41dbaac847538412b31a
pavlo.maksymov <pavlo.maksymov@spryker.com>
2023-03-22 23:20:46 +0100

    Additional dummy change

--- index.php
+++ index.php
@@ -10,5 +10,6 @@ $r = new Randomizer();
 echo($r->randomString('hey', 'hi', 'test') . PHP_EOL);
 
 $bVal = $r->randomBool();
+
 echo "Random Bool: ";
 echo $bVal === false ? 'false' : 'true' . PHP_EOL;
